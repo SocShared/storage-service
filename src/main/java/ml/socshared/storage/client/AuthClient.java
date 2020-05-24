535161b7274bad87d2d2f6b3f2fe310f2181c962
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-24 03:13:52 +0300

    Поправил Feign url

--- src/main/java/ml/socshared/storage/client/AuthClient.java
+++ src/main/java/ml/socshared/storage/client/AuthClient.java
@@ -5,11 +5,12 @@ import ml.socshared.storage.domain.response.SuccessResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "auth-client", url = "${feign.url.auth:}")
 public interface AuthClient {
 
-    @GetMapping(value = "/api/v1/public/service/validate_token", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/api/v1/public/service/validate_token", produces = MediaType.APPLICATION_JSON_VALUE)
     SuccessResponse send(CheckTokenRequest request);
 
 }
