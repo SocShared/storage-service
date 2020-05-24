e4519783541ce6737b39adc37dcb5b5f74899119
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-05-24 03:39:52 +0300

    Добавил проверку роли в контроллеры

--- src/main/java/ml/socshared/storage/controller/v1/GroupController.java
+++ src/main/java/ml/socshared/storage/controller/v1/GroupController.java
@@ -8,6 +8,7 @@ import ml.socshared.storage.domain.response.GroupResponse;
 import ml.socshared.storage.entity.Group;
 import ml.socshared.storage.service.GroupService;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.MediaType;
@@ -20,15 +21,18 @@ import java.util.UUID;
 @RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 @Validated
+@PreAuthorize("isAuthenticated()")
 public class GroupController implements GroupApi {
 
     private final GroupService service;
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/groups/{groupId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public GroupResponse findById(@PathVariable UUID groupId) {
         return service.findById(groupId);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/users/{userId}/groups", produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<GroupModel> findByUserId(@PathVariable UUID userId,
                                                    @NotNull @RequestParam(name = "page", required = false) Integer page,
@@ -36,6 +40,7 @@ public class GroupController implements GroupApi {
         return service.findByUserId(userId, page, size);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/users/{userId}/groups/social_network/{socialNetwork}/groups", produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<GroupModel> findByUserIdAndSocialNetwork(@PathVariable UUID userId, @PathVariable Group.SocialNetwork socialNetwork,
                                                    @NotNull @RequestParam(name = "page", required = false) Integer page,
@@ -43,21 +48,25 @@ public class GroupController implements GroupApi {
         return service.findByUserIdAndSocialNetwork(userId, socialNetwork, page, size);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/users/{userId}/groups/facebook/{facebookId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public GroupResponse findByUserIdAndFacebookId(@PathVariable UUID userId, @PathVariable String facebookId) {
         return service.findByUserIdAndFacebookId(userId, facebookId);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/users/{userId}/groups/vk/{vkId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public GroupResponse findByUserIdAndVkId(@PathVariable UUID userId, @PathVariable String vkId) {
         return service.findByUserIdAndVkId(userId, vkId);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @PostMapping(value = "/private/groups", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public GroupResponse save(@RequestBody @Valid GroupRequest request) {
         return service.save(request);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @DeleteMapping(value = "/private/groups/{groupId}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void deleteById(@PathVariable UUID groupId) {
         service.deleteById(groupId);
--- src/main/java/ml/socshared/storage/controller/v1/PublicationController.java
+++ src/main/java/ml/socshared/storage/controller/v1/PublicationController.java
@@ -7,6 +7,7 @@ import ml.socshared.storage.domain.response.PublicationResponse;
 import ml.socshared.storage.service.PublicationService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +18,19 @@ import javax.validation.constraints.NotNull;
 @RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 @Validated
+@PreAuthorize("isAuthenticated()")
 public class PublicationController {
 
     private final PublicationService publicationService;
 
+    @PreAuthorize("hasRole('SERVICE')")
     @PostMapping(value = "/private/publications", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public PublicationResponse save(@RequestBody @Valid PublicationRequest request) {
         return publicationService.save(request);
     }
 
+    @PreAuthorize("hasRole('SERVICE')")
     @GetMapping(value = "/private/publications/status/not_publishing")
     public Page<PublicationModel> findNotPublishing(@NotNull @RequestParam(name = "page", required = false) Integer page,
                                                     @NotNull @RequestParam(name = "size", required = false) Integer size) {
