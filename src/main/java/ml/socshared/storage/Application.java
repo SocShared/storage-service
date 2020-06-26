106cdd5f199fe2d2686589541fed5207689a61fe
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-26 05:21:57 +0300

    Фикс enable sheduling

--- src/main/java/ml/socshared/storage/Application.java
+++ src/main/java/ml/socshared/storage/Application.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
