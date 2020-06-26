82a12b2ed4fa45b1e919560a65854ad728fdbdd4
Владислав Овчинников <vladovchinnikov950@gmail.com>
2020-06-26 05:41:10 +0300

    Фикс enable sheduling

--- src/main/java/ml/socshared/storage/entity/GroupPostStatus.java
+++ src/main/java/ml/socshared/storage/entity/GroupPostStatus.java
@@ -30,12 +30,12 @@ public class GroupPostStatus implements Serializable {
     private UUID publicationId;
 
     @JsonBackReference
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "publication_id", referencedColumnName = "publication_id", insertable = false, updatable = false)
     private Publication publication;
 
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "group_id", referencedColumnName = "group_id", insertable = false, updatable = false)
     private Group group;
 
